2e3e41b751e1b170559607435ffbc25b2c708815
Akuli <akuviljanen17@gmail.com>
2026-01-18 18:29:34 +0200

    Add --loop option (#7)

--- tests.sh
+++ tests.sh
@@ -41,12 +41,13 @@ function expect() {
 
     local output="$1"
     shift 2
-    diff -u --color <(printf '%s\n' "$output") <("$@" 2>&1) || (echo ""; echo "Command failed: $@"; exit 1)
+    diff -u --color <(printf '%s\n' "$output") <("$@" 2>&1) || (echo ""; echo "Command at $0:$(caller | cut -d' ' -f1) failed: $@"; exit 1)
 
     # One green dot per successful test
     echo -ne "${GREEN}.${RESET}"
 }
 
+# TODO: delete this, use HIMMELI_OVERRIDE_TIME instead
 function remove_details() {
     "$@" | sed 's/[0-9]\.[0-9][0-9]/x.xx/g'
 }
@@ -58,9 +59,10 @@ Usage:
   ./himmeli [options] "STATE1 @ hh:mm" "STATE2 @ hh:mm" ...
 
 Options:
-  --help          this message
-  --allow-dark    allow making the screen very dark
-  --dry-run       don't adjust screens, describe what would be done
+  --help           this message
+  --allow-dark     allow making the screen very dark
+  --loop INTERVAL  adjust, wait, adjust again etc, INTERVAL is e.g. 5min or 1h
+  --dry-run        don't adjust screens, describe what would be done
 
 The simplest way to specify a state is a number between 0 and 1. Here 0 means
 black screen, 1 means reset, and values between 0 and 1 filter out mostly blue
@@ -132,6 +134,72 @@ expect 'Would multiply red by x.xx, green by x.xx and blue by x.xx.' from remove
 expect 'Would multiply red by 1.00, green by 0.00 and blue by 0.00.' from $himmeli "1 0 0"
 expect 'Would multiply red by 0.00, green by 1.00 and blue by 0.00.' from $himmeli "0 1 0"
 expect 'Would multiply red by 0.00, green by 0.00 and blue by 1.00.' from $himmeli "0 0 1"
+
+section "The --loop flag"
+expect './himmeli: invalid loop time "asd", try e.g. 3min or 1h' from $himmeli --loop asd
+expect './himmeli: invalid loop time "asd", try e.g. 3min or 1h' from $himmeli --loop=asd
+expect './himmeli: invalid loop time "", try e.g. 3min or 1h' from $himmeli --loop ""
+expect './himmeli: invalid loop time "", try e.g. 3min or 1h' from $himmeli --loop=
+expect './himmeli: invalid loop time "h", try e.g. 3min or 1h' from $himmeli --loop h
+expect './himmeli: invalid loop time "h", try e.g. 3min or 1h' from $himmeli --loop=h
+expect './himmeli: invalid loop time "123123123123123123h", try e.g. 3min or 1h' from $himmeli --loop 123123123123123123h
+expect './himmeli: invalid loop time "123123123123123123h", try e.g. 3min or 1h' from $himmeli --loop=123123123123123123h
+expect './himmeli: missing time unit in loop time "3", try e.g. 3min or 1h' from $himmeli --loop 3
+expect './himmeli: missing time unit in loop time "3", try e.g. 3min or 1h' from $himmeli --loop=3
+expect './himmeli: loop time cannot be zero' from $himmeli --loop "0 minutes"
+expect './himmeli: loop time cannot be zero' from $himmeli --loop "0 hours"
+expect './himmeli: specifying loop time in seconds is not supported, use minutes or hours' from $himmeli --loop 1s
+expect './himmeli: specifying loop time in seconds is not supported, use minutes or hours' from $himmeli --loop "1 sec"
+expect './himmeli: specifying loop time in seconds is not supported, use minutes or hours' from $himmeli --loop 1second
+expect './himmeli: specifying loop time in seconds is not supported, use minutes or hours' from $himmeli --loop "1 seconds"
+
+HIMMELI_OVERRIDE_TIME=01:00 \
+expect 'Would immediately apply visibility value 1.00.
+Then wait 1 minute (until 01:01).
+Then adjust again: apply visibility value 0.98.
+Then wait 1 minute (until 01:02).
+Then adjust again: apply visibility value 0.96.
+Then wait 1 minute (until 01:03).
+Then adjust again: apply visibility value 0.94.
+Then wait 1 minute (until 01:04).
+Then adjust again: apply visibility value 0.92.
+And so on.' from $himmeli --loop "1 min" "1 @ 1" "0.4 @ 1:30"
+
+HIMMELI_OVERRIDE_TIME=01:00 \
+expect 'Would immediately apply visibility value 1.00.
+Then wait 5 minutes (until 01:05).
+Then adjust again: apply visibility value 0.90.
+Then wait 5 minutes (until 01:10).
+Then adjust again: apply visibility value 0.80.
+Then wait 5 minutes (until 01:15).
+Then adjust again: apply visibility value 0.70.
+Then wait 5 minutes (until 01:20).
+Then adjust again: apply visibility value 0.60.
+And so on.' from $himmeli --loop "5 minutes" "1 @ 1" "0.4 @ 1:30"
+
+HIMMELI_OVERRIDE_TIME=01:00 \
+expect 'Would immediately apply visibility value 0.40.
+Then wait 1 hour and 30 minutes (until 02:30).
+Then adjust again: apply visibility value 0.50.
+Then wait 1 hour and 30 minutes (until 04:00).
+Then adjust again: apply visibility value 0.60.
+Then wait 1 hour and 30 minutes (until 05:30).
+Then adjust again: apply visibility value 0.70.
+Then wait 1 hour and 30 minutes (until 07:00).
+Then adjust again: apply visibility value 0.80.
+And so on.' from $himmeli --loop 90min "0.4 @ 1" "0.8 @ 7"
+
+HIMMELI_OVERRIDE_TIME=01:00 \
+expect 'Would immediately multiply red by 0.80, green by 0.40 and blue by 0.00.
+Then wait 2 hours (until 03:00).
+Then adjust again: multiply red by 0.85, green by 0.55 and blue by 0.15.
+Then wait 2 hours (until 05:00).
+Then adjust again: multiply red by 0.90, green by 0.70 and blue by 0.30.
+Then wait 2 hours (until 07:00).
+Then adjust again: multiply red by 0.95, green by 0.85 and blue by 0.45.
+Then wait 2 hours (until 09:00).
+Then adjust again: multiply red by 1.00, green by 1.00 and blue by 0.60.
+And so on.' from $himmeli --loop=2h "0.8 0.4 0 @ 1" "1 1 0.6 @ 9"
 
 echo ""
 echo ""
